b4e1f6327b0f7bf187b32eea50d95c10ef2ce500
Snehal979 <bansodsnehal535@gmail.com>
2023-03-25 00:37:22 +0530

    [Snehal]Add:Make Two Table for The Join table

--- EmployeePayRollSerivceADO/EmployeePayRollServiceADONET.sql
+++ EmployeePayRollSerivceADO/EmployeePayRollServiceADONET.sql
@@ -65,17 +65,21 @@ End
 --------------------------------------------------------
 drop table employeePayRoleTable
 -------------------------------------------------------
---Uc7
+--Uc8
 --EMPLOYEE TABLE
 CREATE TABLE EmployeeDetail_ado(
 EmpId INT PRIMARY KEY IDENTITY(1,1),
 EmpName VARCHAR(150) NOT NULL,
 Gender VARCHAR(10) NOT NULL,
 );
-
+------------Data insert Uc 8----------------
+INSERT INTO EmployeeDetail_ado(EmpName,Gender)VALUES('Snehal','Female');
+INSERT INTO EmployeeDetail_ado(EmpName,Gender)VALUES('Mayur','Male');
+INSERT INTO EmployeeDetail_ado(EmpName,Gender)VALUES('Raju','Male');
+INSERT INTO EmployeeDetail_ado(EmpName,Gender)VALUES('Lata','Female');
+INSERT INTO EmployeeDetail_ado(EmpName,Gender)VALUES('Vaibhav','Male');
 ---Display---
 SELECT *FROM EmployeeDetail_ado;
-
 -- Salary Table
 CREATE TABLE Salary_ado(
 BasicPay BIGINT DEFAULT(000),
@@ -84,6 +88,13 @@ Tax BIGINT DEFAULT(000),
 EmpId INT FOREIGN KEY REFERENCES EmployeeDetail_ado(EmpId),
 NetPay Bigint 
 );
+INSERT INTO Salary_ado(BasicPay,Deduction,Tax,EmpId) VALUES(10000000,2000,34333,1);
+INSERT INTO Salary_ado(BasicPay,Deduction,Tax,EmpId) VALUES(20000000,5000,3435,2);
 
---Display
+INSERT INTO Salary_ado(BasicPay,Deduction,Tax,EmpId) VALUES(1000000,4000,31333,3);
+
+INSERT INTO Salary_ado(BasicPay,Deduction,Tax,EmpId) VALUES(10000000,2000,34333,4);
+UPDATE Salary_ado SET NetPay = (BasicPay-Tax-Deduction);
 SELECT *FROM Salary_ado;
+SELECT EmpName,NetPay FROM EmployeeDetail_ado AS C
+inner join Salary_ado AS D ON C.EmpId=D.EmpId; 
